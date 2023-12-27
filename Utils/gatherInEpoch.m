18abc7dd0e75bde0e15759ad9a9d1c2f186f7b99
svrajesh <sameervikrajesh@gmail.com>
2023-12-27 11:57:39 -0600

    archive old code to local machine

--- Utils/gatherInEpoch.m
+++
@@ -1,6 +0,0 @@
-function powersEpoch = gatherInEpoch(powers,times,borders)
-
-inds = find(times>=borders(1)+5 & times<=borders(2)-5);
-powersEpoch = powers(inds);
-
-end
--- Utils/nan_demean.m
+++
@@ -1,6 +0,0 @@
-%% Remove the mean of signal and replace nan with 0
-
-function signdm = nan_demean(sig)
-signdm = sig - mean(sig,'omitnan');
-signdm(isnan(signdm)) = 0;
-end
